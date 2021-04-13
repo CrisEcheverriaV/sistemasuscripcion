6dfee65af35c16964db1055e5cdf0ba2b98e0036
unknown <Cris@NB_CECHEVERRIAV.domarzobispado.cl>
2021-04-13 11:12:04 -0400

    Instalacion de Livewire y Laravel Breeze

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,16 @@
 
 namespace Database\Seeders;
 
+use App\Models\Subscriber;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+class DatabaseSeeder extends Seeder {
+	/**
+	 * Seed the application's database.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		\App\Models\Subscriber::factory(10)->create();
+	}
 }
